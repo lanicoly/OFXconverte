1cfc5962b25f6d5ba1a2577bfee0a658d87a66c0
lais <laisnicmns@gmail.com>
2024-08-09 18:59:57 -0300

    conceito ofx + tutorial da conversão

--- src/app.tsx
+++ src/app.tsx
@@ -5,6 +5,10 @@ import { PrimeiraEtapa } from "./componentes/primeiraEtapa";
 import { SegundaEtapa } from "./componentes/segundaEtapa";
 import { TerceiraEtapa } from "./componentes/terceiraEtapa";
 import { QuartaEtapa } from "./componentes/quartaEtapa";
+import { FileEdit } from "./componentes/icons/fileEdit";
+import { FileSave } from "./componentes/icons/fileSave";
+import { Celebration } from "./componentes/icons/celebration";
+
 
 
 export function App() {
@@ -150,7 +154,7 @@ export function App() {
   ];
 
   return (
-    <div>
+    <div className="space-y-4 my-4">
 
       <Menu
         menuItems={menuItems}
@@ -159,15 +163,92 @@ export function App() {
       />
 
       {/* main */}
-      <div className="relative text-xl w-full text-center space-y-6 mt-6 mb-6 top-[80px]">
+      <div className="relative text-xl w-full text-center space-y-8 mt-6 mb-6 top-[80px]">
 
         {/* essa cor 'azulao' é uma cor específica que criei para se adequar ao protótipo */}
         <p className="text-azulao font-medium text-2xl">Sua ferramenta de conversão <strong className="text-roxao font-bold">OFX</strong> favorita</p>
 
         {etapaComponent}
 
+        <div className="flex items-center flex-col justify-center gap-3 my-6">
+            <h1 className="font-semibold text-3xl mt-4">O QUE É EXTRATO <strong className="text-roxao font-bold">OFX</strong>?</h1>
+
+             <div className="flex mx-80 mt-4 ">
+                <div className="flex w-3 bg-gradient-to-b from-azul-logo to-roxao shadow-shape"></div> {/* barra lateral degrade */}
+                <div className=" bg-white px-4 py-4 rounded-e-lg shadow-shape flex-1 text-left">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam, labore rerum consequuntur in perspiciatis, explicabo eos excepturi, nisi impedit nihil sapiente? Quo, perspiciatis repellendus inventore ullam quia iste eaque mollitia!
+                </div>
+            </div>
+
+             <div className="flex mx-80 mt-4">
+                <div className="flex w-3 bg-gradient-to-b from-azul-logo to-roxao shadow-shape"></div> {/* barra lateral degrade */}
+                <div className=" bg-white px-4 py-4 rounded-e-lg shadow-shape text-left flex-1">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam, labore rerum consequuntur in perspiciatis, explicabo eos excepturi, nisi impedit nihil sapiente? Quo, perspiciatis repellendus inventore ullam quia iste eaque mollitia!
+                </div>
+            </div>
+
+        </div>
+
+        <div className='bg-white w-[1040px] m-auto rounded-xl py-6 px-6 gap-6 shadow-shape'>
+          <h1 className="font-semibold text-3xl my-3">TUTORIAL DE CONVERSÃO <strong className="text-roxao font-bold">OFX</strong></h1>
+          <div className=" flex-wrap flex my-6 justify-center gap-7">
+            
+            {/* card */}
+            <div className="flex flex-col gap-1">
+              <div className="px-4 py-1.5 rounded-t-lg bg-gradient-to-r from-azul-logo to-roxao border-none text-white font-semibold text-lg w-96">Passo 1</div>
+              <div className="px-3 py-2 rounded-b-lg bg-sky-100 flex gap-3 w-96 justify-center items-center">
+                <div className="size-12 rounded-lg bg-white flex justify-center items-center">
+                  <i className="bi bi-file-earmark-arrow-up-fill text-azulao text-3xl"></i>
+                </div>
+                <p className="text-base text-azulao font-medium flex-1 text-left leading-5 py-2 flex items-center justify-center">Selecione o tipo do arquivo que deseja converter e logo após escolha um ou mais arquivos para realizar a conversão</p>
+              </div>
+            </div>
+            {/* fim card */}
+
+            {/* card 2 */}
+            <div className="flex flex-col gap-1">
+              <div className="px-4 py-1.5 rounded-t-lg bg-gradient-to-r from-azul-logo to-roxao border-none text-white font-semibold text-lg w-96">Passo 2</div>
+              <div className="px-3 py-2 rounded-b-lg bg-sky-100 flex gap-3 w-96 justify-center items-center">
+                <div className="size-12 rounded-lg bg-white flex justify-center items-center">
+                  <FileEdit color="#0078a7" size={36} />
+                </div>
+                <p className="text-base text-azulao font-medium flex-1 text-left leading-5 py-2 flex items-center justify-center">Visualize, edite ou exclua os arquivos escolhidos e adicione mais arquivos caso necessário</p>
+              </div>
+            </div>
+            {/* fim card */}
+            
+            {/* card 3 */}
+            <div className="flex flex-col gap-1">
+              <div className="px-4 py-1.5 rounded-t-lg bg-gradient-to-r from-azul-logo to-roxao border-none text-white font-semibold text-lg w-96">Passo 3</div>
+              <div className="px-3 py-2 rounded-b-lg bg-sky-100 flex gap-3 w-96 justify-center items-center">
+                <div className="size-12 rounded-lg bg-white flex justify-center items-center">
+                  <FileSave color="#0078a7" size={32} />
+                </div>
+                <p className="text-base text-azulao font-medium flex-1 text-left leading-5 py-2 flex items-center justify-center">Visualize e edite o resultado da conversão em OFX e faça o download</p>
+              </div>
+            </div>
+            {/* fim card */}
+            
+            {/* card 4 */}
+            <div className="flex flex-col gap-1">
+              <div className="px-4 py-1.5 rounded-t-lg bg-gradient-to-r from-azul-logo to-roxao border-none text-white font-semibold text-lg w-96">Passo 4</div>
+              <div className="px-3 py-2 rounded-b-lg bg-sky-100 flex gap-3 w-96 justify-center items-center">
+                <div className="size-12 rounded-lg bg-white flex justify-center items-center">
+                  <Celebration color="#0078a7" size={32} />
+                </div>
+                <p className="text-base text-azulao font-medium flex-1 text-left leading-5 py-2 flex items-center justify-center">Conclusão da conversão realizada e direcionamento para o início</p>
+              </div>
+            </div>
+            {/* fim card */}
+
+
+          </div>
+
+        </div>
+      
       </div>
       {/* fim main */}
+
 
     </div>
 
---
+++ src/componentes/conceitoOFX.tsx
@@ -0,0 +1,25 @@
+
+
+export function ConceitoOFX () {
+    return (
+        <div className="flex items-center flex-col justify-center gap-3">
+            <h1 className="font-semibold text-3xl mt-4">O QUE É EXTRATO <strong className="text-roxao font-bold">OFX</strong>?</h1>
+
+             <div className="flex mx-80 mt-4 ">
+                <div className="flex w-3 bg-gradient-to-b from-azul-logo to-roxao shadow-shape"></div> {/* barra lateral degrade */}
+                <div className=" bg-white px-4 py-4 rounded-e-lg shadow-shape flex-1 text-left">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam, labore rerum consequuntur in perspiciatis, explicabo eos excepturi, nisi impedit nihil sapiente? Quo, perspiciatis repellendus inventore ullam quia iste eaque mollitia!
+                </div>
+            </div>
+
+             <div className="flex mx-80 mt-4">
+                <div className="flex w-3 bg-gradient-to-b from-azul-logo to-roxao shadow-shape"></div> {/* barra lateral degrade */}
+                <div className=" bg-white px-4 py-4 rounded-e-lg shadow-shape text-left flex-1">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam, labore rerum consequuntur in perspiciatis, explicabo eos excepturi, nisi impedit nihil sapiente? Quo, perspiciatis repellendus inventore ullam quia iste eaque mollitia!
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
